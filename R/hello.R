1fd9a89efb8650d39192033d78372565ad7d6637
Chavi Gupta <chavig080@gmail.com>
2020-02-27 00:57:05 -0800

    create a simple r compendium

---
+++ R/hello.R
@@ -0,0 +1,31 @@
+
+#' Function to add two numbers
+#'
+#' @param a
+#' @param b
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' add_two_numbers(1, 3)
+#' add_two_numbers(-5, 6)
+add_two_numbers <- function(a, b) {
+  return(a+b)
+}
+
+
+#' Function to multiply two numbers
+#'
+#' @param a
+#' @param b
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' multiply_two_numbers(4, 5)
+#' multiply_two_numbers(6, -3)
+multiply_two_numbers <- function(a, b) {
+  return(a*b)
+}
